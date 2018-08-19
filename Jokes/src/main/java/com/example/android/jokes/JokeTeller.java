33104bc9092da6720326368d5dec51a604a85c41
Brandon Fadairo <BrandonFadairo@hotmail.com>
2018-08-19 14:59:56 -0700

    First Commit + Added Joke Library Files

---
+++ Jokes/src/main/java/com/example/android/jokes/JokeTeller.java
@@ -0,0 +1,13 @@
+package com.example.android.jokes;
+
+import java.util.List;
+
+public class JokeTeller {
+
+    //For Future Use
+    //private static List jokes;
+
+    public static String getJoke(){
+        return "Guess what?, Chicken Butt";
+    }
+}
